5a723f55cb2d5ad9147bb77423d8fbbff0f660bd
Brandes98 <86222257+Brandes98@users.noreply.github.com>
2021-06-23 20:17:11 -0600

    Add files via upload

--- DEATHPIKE RACER.py
+++ DEATHPIKE RACER.py
@@ -2,30 +2,30 @@ import pygame
 import random
 from math import tan, radians, degrees, copysign
 from pygame.math import Vector2
-class Cactus(pygame.sprite.Sprite):
+class Arbol(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image=pygame.image.load()
+        self.image=pygame.image.load("imagenes proyercto 2/ Arbol.png")
         self.rect=self.image.get_rect()
 
 class minas(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image=pygame.image.load()
+        self.image=pygame.image.load("imagenes proyercto 2/ Mina.png")
         self.rect=self.image.get_rect()
 class meta(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.x=500
         self.y=145
-        self.image= pygame.image.load("imagenes/MetaPista1.png")
+        self.image= pygame.image.load("imagenes proyercto 2/ Arbol.png")
         self.rect= self.image.get_rect()
         self.rect.center=(self.x,self.y)
  #clase jugador 1
 class Jugador1(pygame.sprite.Sprite):
     def __init__(self, x=220, y=150, angulo=0.0, largoVehiculo=3, maxima_rotacion=30, maxima_aceleracion=200.0):
         super().__init__()
-        self.imagen = pygame.image.load("imagenes/RedCar.png")
+        self.imagen = pygame.image.load("imagenes proyercto 2/ enemies_level1.png")
         self.rect = self.imagen.get_rect()
         self.rect.center = (x, y)
         self.posicion = Vector2(x, y)
@@ -56,6 +56,41 @@ class Jugador1(pygame.sprite.Sprite):
 
         self.posicion += self.velocidad.rotate(-self.angulo) * dt
         self.angulo += degrees(velocidad_angular) * dt
+class Jugador2(pygame.sprite.Sprite):
+    def __init__(self, x=130, y=95, angulo=0.0, largoVehiculo=3, maxima_rotacion=30, maxima_aceleracion=200.0):
+        super().__init__()
+        self.posicion = Vector2(x, y)
+        self.velocidad = Vector2(0.0, 0.0)
+        self.image = pygame.image.load("imagenes proyercto 2/ enemies2_level1.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+        self.angulo = angulo
+        self.largoVehiculo = largoVehiculo
+        self.maxima_aceleracion = maxima_aceleracion
+        self.maxima_rotacion = maxima_rotacion
+        self.maxima_velocidad = 200
+        self.romper_desaceleracion = 200
+        self.desaceleracion_libre = 2
+        self.arranque = pygame.mixer.Sound("sonido/acelerar.mp3")
+        self.frenado= pygame.mixer.Sound("sonido/frenando.mp3")
+        self.puntaje=0
+        self.aceleracion =100.0
+        self.rotacion = 0.0
+ #actualiza el jugador2
+    def update(self, dt2):
+        self.velocidad += (self.aceleracion * dt2, 0)
+        self.velocidad.x = max(-self.maxima_velocidad, min(self.velocidad.x, self.maxima_velocidad))
+        self.rect.x=self.posicion.x
+        self.rect.y=self.posicion.y
+        if self.rotacion:
+            radio_rotacion = self.largoVehiculo / tan(radians(self.rotacion))
+            velocidad_angular = self.velocidad.x / radio_rotacion
+        else:
+            velocidad_angular = 0
+
+        self.posicion += self.velocidad.rotate(-self.angulo) * dt2
+        self.angulo += degrees(velocidad_angular) * dt2
+
 class Juego:
     def __init__(self):
         pygame.init()
@@ -65,18 +100,17 @@ class Juego:
         ALTURA = 720
         self.reloj= pygame.time.get_ticks()//1000
         self.screen = pygame.display.set_mode((ANCHO,ALTURA))
+
         self.clock = pygame.time.Clock()
         self.ticks = 60
         self.exit = False
-        self.pista1=pygame.image.load("imagenes/pista1.PNG")
-        self.pista1D = pygame.image.load("imagenes/pista1borders.png")
-        self.pista2= pygame.image.load("imagenes/pista2.png")
-        self.pista2D = pygame.image.load("imagenes/pista2borders.png")
+        self.pista1=pygame.image.load("imagenes proyercto 2/ modelo fondo.png")
         def level1(self):
             jugador1 = Jugador1()
-            Meta=meta()
+            jugador2 = Jugador2()
+            Meta = meta()
             Minas=minas()
-            cactus=Cactus()
+            arbol=Arbol()
             pressed = pygame.key.get_pressed()
 
             if pressed[pygame.K_UP]:
@@ -158,16 +192,21 @@ class Juego:
                 Minas.rect.x= random.randrange(ALTURA)
                 Minas.rect.y = random.randrange(ANCHO)
             for catus in range(12):
-                cactus.rect.x= random.randrange(ALTURA)
-                cactus.rect.y = random.randrange(ANCHO)
+                arbol.rect.x= random.randrange(ALTURA)
+                arbol.rect.y = random.randrange(ANCHO)
 
             objetos = pygame.sprite.Group()
             objetos.add(Meta)
             carros=pygame.sprite.group()
             carros.add(jugador1)
-            carros.add(jugador2)
-            choque = pygame.sprite.spritecollide(cactus, carros, True)
-            if jugador1.vida==0:
+            carros2.
+            carros2.add(jugador2)
+            choque = pygame.sprite.spritecollide(arbol, carros, True)
+            if choque:
+                metasound = pygame.mixer.Sound("sonido/meta.mp3")
+                metasound.play()
+                jugador1.vida-=1000
+            if jugador1.vida<=0:
                 jugador1.destroy()
             if choque:
                 explosion=pygame.mxer.sound("sonido/explosion.mp3")
@@ -187,7 +226,6 @@ class Juego:
             jugador1.update(dt)
             jugador2.update(dt2)
             self.screen.blit(self.pista1, (0, 0))
-            self.screen.blit(self.pista1D, (0, 0))
             rotado = pygame.transform.rotate(jugador1.imagen, jugador1.angulo)
             rect = rotado.get_rect()
             self.screen.blit(rotado, jugador1.posicion)
@@ -213,6 +251,5 @@ class Juego:
             pygame.display.flip()
 
             self.clock.tick(self.ticks)
-if __name__=__main__:
+if __name__=="__main__":
     juego=Juego()
-    
